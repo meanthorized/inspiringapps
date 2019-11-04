b3980ae11b5df5d3cb935d7169a1ed12ab112443
Angie Anthor <angieanthor@gmail.com>
2019-11-03 23:48:06 -0600

    Fixed bug and added alt and titles to images

--- src/scripts/Dot.js
+++ src/scripts/Dot.js
@@ -4,31 +4,41 @@ export let Dot = (function(){
 			id: 'blue-dot',
 			color: 'blue',
 			classList: 'dot-img blue',
-			src: 'img/ia-logo-dot-blue.png'
+			src: 'img/ia-logo-dot-blue.png',
+			alt: 'Blue dot',
+			title: 'Drag me!'
 		},
 		{
 			id: 'red-dot',
 			color: 'red',
 			classList: 'dot-img red',
-			src: 'img/ia-logo-dot-red.png'
+			src: 'img/ia-logo-dot-red.png',
+			alt: 'Red dot',
+			title: 'Drag me!'
 		},
 		{
 			id: 'green-dot',
 			color: 'green',
 			classList: 'dot-img green',
-			src: 'img/ia-logo-dot-green.png'
+			src: 'img/ia-logo-dot-green.png',
+			alt: 'Green dot',
+			title: 'Drag me!'
 		},
 		{
 			id: 'black-dot',
 			color: 'black',
 			classList: 'dot-img black',
-			src: 'img/ia-logo-dot-black.png'
+			src: 'img/ia-logo-dot-black.png',
+			alt: 'Black dot',
+			title: 'Drag me!'
 		},
 		{
 			id: 'black-dot-2',
 			color: 'black',
 			classList: 'dot-img black',
-			src: 'img/ia-logo-dot-black.png'
+			src: 'img/ia-logo-dot-black.png',
+			alt: 'Another black dot',
+			title: 'Drag me!'
 		}
 	];
 
@@ -37,12 +47,14 @@ export let Dot = (function(){
 		let counter = 0;
 
 		sourceContainers.forEach((container) => {
-			let {id, classList, src} = dots[counter];
+			let {id, classList, src, alt, title} = dots[counter];
 			let dotElement = document.createElement('img');
 			
 			dotElement.setAttribute('id', id);
 			dotElement.setAttribute('src', src);
 			dotElement.setAttribute('class', classList);
+			dotElement.setAttribute('alt', alt);
+			dotElement.setAttribute('title', title);
 			container.appendChild(dotElement);
 
 			counter++;
